3f37ee616c7bb92170d49b0237d239c5b421e725
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2018-05-10 00:11:20 -0300

    Inventario quedaría así

--- src/estado.js
+++ src/estado.js
@@ -9,9 +9,11 @@ const estado = {
     objetoSeleccionado: null
   },
   detener: false,
-  niveles: [],
-  nivelActual: null,
   dialogo: null,
+  niveles: {
+    niveles: [],
+    nivelActual: null,
+  },
   movilidad: {
     norte: 0,
     sur: 0,
@@ -29,7 +31,19 @@ const genericSet = collection => (insertionSet) => {
 export const getAcciones = genericGet('acciones')
 export const getMovilidad = genericGet('movilidad')
 export const getInventario = genericGet('inventario')
+export const getNiveles = genericGet('niveles')
 
 export const setAcciones = genericSet('acciones')
 export const setMovilidad = genericSet('movilidad')
 export const setInventario = genericSet('inventario')
+
+export const setNiveles = niveles => {
+  if (estado.niveles.niveles.length) return false
+  estado.niveles.niveles = niveles
+  return estado.niveles
+}
+
+export const setNivelActual = nivelActual => {
+  estado.niveles.nivelActual = nivelActual
+  return estado.niveles
+}
--- src/inventario.js
+++ src/inventario.js
@@ -1,82 +1,48 @@
 // Generadores de fichas de invetario
+import { getInventario, setInventario, detenido } from './estado'
 
-function objetoi (nombre) {
-  objetoi.count++
-  inventa[objetoi.count] = nombre
-  const boton = `<input type="image" id="${nombre}" onclick="accionficha('${nombre}')" src="./img/obj/${nombre}-o.png" class="objeto${objetoi.count}o" />`
-  divPrincipal.insertAdjacentHTML('afterbegin', boton)
+export function agregarObjeto (objeto) {
+    if (detenido()) return undefined;
+
+    const { objetos } = getobjetos()
+    objetos.push(objeto)
+    return setobjetos({ objetos })
 }
 
-function accionficha (nombre) {
-  if (detener) return undefined
+export function seleccionarObjeto (objeto) {
+    if (detenido()) return undefined;
 
-  if (usar) {
-    if (usaro1 == null) {
-      const index = inventa.findIndex(objInventario => objInventario === nombre)
-      if (index !== -1) {
-        selecFicha(nombre, index)
-      }
-    } else {
-      usaro2 = nombre
-      accionUsar()
-    }
-  }
-
-  if (mirar) {
-    mirar = 0
-    cuadromirar(nombre)
-  }
-  actualizarAcciones(tomar, mirar, usar)
-}
-
-// ordenador de inventario
-
-function inventario () {
-  let a = 1
-  let test
-  let test2
-  let elqsigue
-
-  for (var indice = 0; indice < inventa.length; indice++) {
-    if (inventa[indice]) continue
-    let siguienteIndice = indice + 1
-    let siguienteElemento = inventa[indice + 1]
-    if (siguienteElemento) {
-      document.getElementById(siguienteElemento).className = `objeto${indice}o`
-      inventa[siguienteIndice] = null
-      inventa[indice] = siguienteElemento
-    }
-  }
-}
-
-function selecFicha (nombre, indice) {
-  console.log('entro en seleccion')
-  if (!seleccion) {
-    console.log('es 0')
-    console.log(inventa)
-    seleccion = 1
-    const imgsele = `<input type="image" src="./img/fichasel.png" id="pepito" class="objeto${indice}o always-on-top" />`
-    console.log(imgsele)
-    divPrincipal.insertAdjacentHTML('afterbegin', imgsele)
-
-    usaro1 = nombre
+    let { objetoSeleccionado } = getInventario()
+    objetoSeleccionado = objeto
     asf.play()
-  } else {
-    console.log('es 1')
-    seleccion = 0
-    const botone = document.getElementById('pepito')
-    divPrincipal.removeChild(botone)
-  }
+    return setInventario({ objetoSeleccionado })
 }
 
-function eliminar (nombre) {
-  const botone = document.getElementById(nombre)
-  divPrincipal.removeChild(botone)
+export function quitarObjeto (objeto) {
+    if (detenido()) return undefined;
 
-  const index = inventa.findIndex(obj => obj === nombre)
-  if (index === -1) return undefined
+    let { objetos, objetoSeleccionado } = getInventario()
+    if (objeto === objetoSeleccionado) objetoSeleccionado = null
+    objetos = objetos.filter(x => x !== objeto)
+    return setInventario({ objetos, objetoSeleccionado })
+}
 
-  inventa[index] = null
-  objetoi.count--
-  inventario()
+export const renderObjeto = (objeto, seleccionado) =>
+    `<input type="image" class="objeto${seleccionado ? ' seleccionado' : ''}" id="${objeto.nombre}" src="./img/obj/${objeto.nombre}.png" />`
+
+export const renderInventario = () => {
+    const { objetos, objetoSeleccionado } = getInventario()
+    return objetos.map(x => renderObjeto(x, x === objetoSeleccionado)).join('\n')
+}
+
+export function refreshInventario () {
+    const divInventario = pruneNode(document.getElementById('inventario'))
+    divInventario.insertAdjacentHTML('afterbegin', renderInventario())
+}
+
+
+const pruneNode = node => {
+    node.innerHTML = ''
+    while (node.hasChildNodes()) node.removeChild(node.firstChild)
+    return node
 }
